66578ad62b1588a542c127ed2aff46898c66bfd7
Sana Mumtaz <35191030+sanamumtaz@users.noreply.github.com>
2021-08-29 22:41:14 +0500

    remove record video

--- src/components/BokehEffectRecording.js
+++ src/components/BokehEffectRecording.js
@@ -3,13 +3,13 @@ import * as tf from "@tensorflow/tfjs"
 import * as bodyPix from "@tensorflow-models/body-pix"
 
 export const BokehEffectRecording = () => {
-  const [isRecording, setIsRecording] = useState(false)
+  //const [isRecording, setIsRecording] = useState(false)
   const [audioTrack, setAudioTrack] = useState(null)
   const frameId = useRef(null)
   const canvasReference = useRef(null)
   const videoRef = useRef(null)
-  const recordedVideoRef = useRef(null)
-  const mediaRecorderRef = useRef(null)
+  // const recordedVideoRef = useRef(null)
+  // const mediaRecorderRef = useRef(null)
 
   const setupCamera = useCallback(async (videoElement) => {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -66,24 +66,24 @@ export const BokehEffectRecording = () => {
     segmentBodyAndBlur(videoElement, canvasElement, bodypixnet)
   }
 
-  const handleCaptureStream = useCallback(() => {
-    setIsRecording(true)
-    const stream = canvasReference.current.captureStream()
-    stream.addTrack(audioTrack)
-    mediaRecorderRef.current = new MediaRecorder(stream, {
-      mimeType: "video/webm",
-    })
-    mediaRecorderRef.current.addEventListener("dataavailable", (evt) => {
-      const url = URL.createObjectURL(evt.data)
-      recordedVideoRef.current.src = url
-    })
-    mediaRecorderRef.current.start()
-  }, [canvasReference, mediaRecorderRef, setIsRecording, audioTrack])
+  // const handleCaptureStream = useCallback(() => {
+  //   setIsRecording(true)
+  //   const stream = canvasReference.current.captureStream()
+  //   stream.addTrack(audioTrack)
+  //   mediaRecorderRef.current = new MediaRecorder(stream, {
+  //     mimeType: "video/webm",
+  //   })
+  //   mediaRecorderRef.current.addEventListener("dataavailable", (evt) => {
+  //     const url = URL.createObjectURL(evt.data)
+  //     recordedVideoRef.current.src = url
+  //   })
+  //   mediaRecorderRef.current.start()
+  // }, [canvasReference, mediaRecorderRef, setIsRecording, audioTrack])
 
-  const handleStopCaptureClick = useCallback(() => {
-    mediaRecorderRef.current.stop()
-    setIsRecording(false)
-  }, [mediaRecorderRef, setIsRecording])
+  // const handleStopCaptureClick = useCallback(() => {
+  //   mediaRecorderRef.current.stop()
+  //   setIsRecording(false)
+  // }, [mediaRecorderRef, setIsRecording])
 
   //cancel requestAnimationFrame on unmount
   const cleanUpFrames = useCallback(() => {
@@ -115,12 +115,12 @@ export const BokehEffectRecording = () => {
           zindex: 9,
         }}
       />
-      <video ref={recordedVideoRef} controls></video>
+      {/* <video ref={recordedVideoRef} controls></video>
       <button
         onClick={isRecording ? handleStopCaptureClick : handleCaptureStream}
       >
         {isRecording ? "Stop" : "Start"}
-      </button>
+      </button> */}
     </>
   )
 }
